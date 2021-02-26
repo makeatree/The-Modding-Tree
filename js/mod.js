13fbdbefd79d6f577827168bf8dea854326fc484
aden0216 <79700296+aden0216@users.noreply.github.com>
2021-02-26 21:45:20 +0800

    Test

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Number Tree",
+	id: "AAAAAA",
+	author: "3^3=27",
+	pointsName: "Number",
 	discordName: "",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
